9f47fc0167c67f4d59636d6a2e030eb91e714210
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-03 05:55:04 +0330

    Function: timeToMixJuice

--- mixed-juices.js
+++ mixed-juices.js
@@ -1,1 +1,24 @@
 // look back again 🙂🙂🙂
+
+function timeToMixJuice(name) {
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+      break;
+    case "Energizer":
+    case "Green Garden":
+      return 1.5;
+      break;
+    case "Tropical Island":
+      return 3;
+      break;
+    case "All or Nothing":
+      return 5;
+      break;
+    default:
+      return 2.5;
+  }
+}
+// console.log(timeToMixJuice('Tropical Island'));
+// console.log(timeToMixJuice('Berries & Lime'));
+// console.log(timeToMixJuice('Green Garden'));
