768660a3b8abc28f7b647a84fe872e614908c87d
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-03 05:55:47 +0330

    Function: limesToCut

--- mixed-juices.js
+++ mixed-juices.js
@@ -22,3 +22,30 @@ function timeToMixJuice(name) {
 // console.log(timeToMixJuice('Tropical Island'));
 // console.log(timeToMixJuice('Berries & Lime'));
 // console.log(timeToMixJuice('Green Garden'));
+
+///----///
+
+function limesToCut(wedgesNeeded, limes) {
+  let count = 0;
+  while (limes.length > 0 && wedgesNeeded > 0) {
+    switch (limes[0]) {
+      case "small":
+        wedgesNeeded -= 6;
+        break;
+      case "medium":
+        wedgesNeeded -= 8;
+        break;
+      case "large":
+        wedgesNeeded -= 10;
+        break;
+      default:
+        console.log("error");
+        break;
+    }
+    count++;
+    limes.splice(0, 1);
+  }
+  return count;
+}
+
+// console.log(limesToCut(25, ['small', 'small', 'large', 'medium', 'small']));
